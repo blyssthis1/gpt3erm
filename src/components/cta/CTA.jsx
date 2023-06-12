22c784c36108120788c57d089fab5fe11d2c4db0
Ermias Drar <wifiwork2@gmail.com>
2023-06-12 14:40:15 -0700

    first commit

---
+++ src/components/cta/CTA.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './cta.css';
+
+
+const CTA = () =>{
+  return (
+    <div>
+    
+        CTA
+
+    </div>
+  )
+}
+
+export default CTA
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { RiMenu3Line, RiCloseLin } from 'react-icons/ri'
+import logo from '../../assets/logo.svg'
+import './navbar.css';
+
+//BEm -> Block Element Modifier
+const Navbar = () =>{
+  return (
+    <div className='gpt3__navbar'>
+      <div className='gpt3__navbar-links'>
+        <div className='gpt3__navbar-links-logo'>
+            <img src="{logo}" alt="logo" />
+        </div>
+
+      </div>
+    
+        Navbar
+
+    </div>
+  )
+}
+
+export default Navbar
